7013b1c761f339a7a2a0d94bf40509ff279c4ed7
Jose Luis <117080231+jlla-official@users.noreply.github.com>
2023-04-10 12:24:44 +0200

    Update app.component.ts

--- Angular/app1/src/app/app.component.ts
+++ Angular/app1/src/app/app.component.ts
@@ -1,24 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Persona } from './persona.model';
+import { LogginService } from './LogginService.service';
+import { PersonasService } from './personas.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   
   titulo = 'Listado de Personas';
 
-  personas : Persona[] = [
-    new Persona('Juan', 'Perez'), 
-    new Persona('Laura', 'Juarez')
-  ];
+  personas : Persona[] = [];
 
-   personaAgregada(persona : Persona){
-    
-    this.personas.push( persona );
+  //Aquí estamos inyectando nuestros servicios (librerias?), para poder usar sus atributos y métodos
+  constructor(private loginService : LogginService,
+              private personasService : PersonasService){
 
-   }
+  }
+  //Con ngOnInit, hacemos que al levantar la app, se ejecute este método
+  ngOnInit(): void {
+    this.personas = this.personasService.personas;
+  }
+
+  
 
 }
