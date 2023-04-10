2521bff2d03e096c515e0c03b83d9a8b2494fdd7
Jose Luis <117080231+jlla-official@users.noreply.github.com>
2023-04-10 13:54:23 +0200

    Update personas.service.ts

--- Angular/app1/src/app/personas.service.ts
+++ Angular/app1/src/app/personas.service.ts
@@ -1,9 +1,16 @@
+import { Injectable } from "@angular/core";
 import { LogginService } from "./LogginService.service";
 import { Persona } from "./persona.model";
 
 /*Los servicios son como las librerías en Java, si se inyectan desde el constructor en la clase que las vamos a usar, podemos acceder a sus métodos y propiedades.
 Ver ejemplos en el componente formulario.component.ts*/
 
+/*En este servicio de Personas, estamos instanciando el servicio LogginService, de esta manera, cuando llamemos al servicio Personas, también podemos llamar
+al servicio LogginService sin tenerlos que llamar por separado.
+
+Para hacer esto, hay que colocar el decorador @Injectable() que hay debajo
+*/
+@Injectable()
 export class PersonasService{
 
     personas : Persona[] = [
@@ -11,8 +18,10 @@ export class PersonasService{
         new Persona('Laura', 'Juarez')
     ];
 
+    constructor(private logginService : LogginService){}
 
     agregarPersona(persona : Persona){
-        this.personas.push( persona );
+      this.logginService.enviaMensajeAConsola("agregamos persona:" + persona)
+      this.personas.push( persona );
     }
 }
