c774c19b8f193d4356d80da53340d476ba1fee33
Jose Luis <117080231+jlla-official@users.noreply.github.com>
2023-04-11 14:01:20 +0200

    Update formulario.component.ts

--- Angular/app1/src/app/formulario/formulario.component.ts
+++ Angular/app1/src/app/formulario/formulario.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 import { Persona } from '../persona.model';
 import { LogginService } from '../LogginService.service';
 import { PersonasService } from '../personas.service';
@@ -10,12 +10,32 @@ import { PersonasService } from '../personas.service';
   providers: [LogginService]
 
 })
+
 export class FormularioComponent {
 
-  //@Output() personaCreada = new EventEmitter<Persona>();
+  /*La variable de abajo personaCreada no esta siendo usada porque no hace falta, pero con el decorador @Output() y el new EventEmitter<tipodeDatoQueEmite>,
+  podemos pasar el valor de la variable personaCreada al archivo .ts del componente padre, app.component.ts
+
+  El flujo sería el siguiente:
+  
+  - Creamos la variable con el nombre que queramos y con el decorador detrás de @Output(), 
+  - Como valor, le instanciamos el objeto EventEmitter<TipoDeDatoQueEmite>()
+  - En el .html del componente padre (app.component.html), usamos un elemento html para crear la conexión entre padre e hijo
+    con la siguiente sintáxis: 
+    
+    (personaCreada) = "métodoDeLArchivoTsPadre($event)" 
+    
+    Le estamos pasando el valor de personaCreada por agumento al métodoDelArchivoTsPadre.
+
+    Y por último procesamos ese valor desde el .ts del componente padre de la manera que queramos.
+  
+  */
+  @Output() personaCreada = new EventEmitter<Persona>();
   
   /*
-  Como en el formulario.component.html hemos usado el término de local reference, mirar el código allí. Ahorramos el tener que declarar variables para almacenar
+  Como en el formulario.component.html hemos usado el término de local reference, mirar el código allí. 
+  
+  Con esto ahorramos el tener que declarar variables para almacenar
   el contenido de un input, por eso está entre comentarios nombreInput y apellidoInput. Sin embargo, haciendo esto, solo podemos extraer el valor de los input,
   porque no hay variables creadas y porque no se está usando el ngModel
   */
@@ -24,7 +44,7 @@ export class FormularioComponent {
   //apellidoInput : string = '';
 
 
-  //Con ViewChild estamos asignando el identificador del input a una variable de la clase con el nombre que queramos, su sintáxis debe ser como abajo
+  //Con ViewChild estamos asignando el identificador del input a una variable con el nombre que queramos, su sintáxis debe ser como abajo
   @ViewChild('nombreInput') nombreInput : ElementRef;
   @ViewChild('apellidoInput') apellidoInput : ElementRef;
 
@@ -34,7 +54,15 @@ export class FormularioComponent {
   */
 
   constructor(private loginService : LogginService,
-    private personasService : PersonasService){}
+    private personasService : PersonasService){
+
+      /*Cuando el evento saludar emite algo, con .subscribe lo recoge, y con una función flecha y creando una variable como argumento, le asignamos el valor
+       emitido a la variable, para luego en el cuerpo del método hacer lo que queramos. Ejemplo abajo*/
+      this.personasService.saludar.subscribe(
+        (indice : number) => alert("El indice es: " + indice)
+      ); 
+      
+  }
 
   /*
     FORMA 1 DE EXTRAER EL VALOR DE UN INPUT (Local Reference)
@@ -53,7 +81,7 @@ export class FormularioComponent {
     onAgregarPersona(){
       let persona1 = new Persona(this.nombreInput.nativeElement.value, this.apellidoInput.nativeElement.value);
       //Aquí abajo en loginService, estamos usando la clase que hemos instanciado en el constructor, para su método, así como una libreria.
-      this.loginService.enviaMensajeAConsola("Enviamos persona: " + persona1.nombre + " " + persona1.apellido);      
+      //this.loginService.enviaMensajeAConsola("Enviamos persona: " + persona1.nombre + " " + persona1.apellido);      
       //this.personaCreada.emit(persona1); 
       this.personasService.agregarPersona(persona1);
     }
